2317e48432955fe12277d1958b68919228e4aae0
Dhia1111 <dhia23445@gmail.com>
2025-02-17 23:22:06 +0100

    Deleting jason files from reposetory and make them git ignore

--- Ecommerce1/obj/Debug/net8.0/Ecommerce1.AssemblyInfo.cs
+++ Ecommerce1/obj/Debug/net8.0/Ecommerce1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fba85337a350470b191b1d84a1d03b6dec216d08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccf148c3127a5519914effa983c993531d519ea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
